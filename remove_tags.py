431ff40e5fc3e770f503d87e7f75b3d0c77373c6
Clemens <clemens9103@gmail.com>
2022-11-03 17:40:47 +0900

    2022-11-03

---
+++ remove_tags.py
@@ -0,0 +1,26 @@
+# Import Module
+from bs4 import BeautifulSoup
+import requests
+  
+# Website URL
+URL = 'https://www.geeksforgeeks.org/data-structures/'
+  
+# Page content from Website URL
+page = requests.get(URL)
+  
+# Function to remove tags
+def remove_tags(html):
+  
+    # parse html content
+    soup = BeautifulSoup(html, "html.parser")
+  
+    for data in soup(['style', 'script']):
+        # Remove tags
+        data.decompose()
+  
+    # return data by retrieving the tag content
+    return ' '.join(soup.stripped_strings)
+  
+  
+# Print the extracted data
+print(remove_tags(page.content))
