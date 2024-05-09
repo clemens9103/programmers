b8b69d1aa961efffe6019303cfca21d21fa79da8
clemens9103 <clemens9103@gmail.com>
2024-05-09 14:05:39 +0900

    GPT Code Updated

--- ChatGPT_Example/gpt_example.py
+++ ChatGPT_Example/gpt_example.py
@@ -1,32 +1,38 @@
 from openai import OpenAI
 import configparser as ConfigParser
 import sys
+import os
+
 class ChatGPT:
     def __init__(self, section, config_file):
         self.cfg = ConfigParser.ConfigParser()
         self.section = section
         self.cfg.read(config_file)
-
         self.key = self.cfg.get('GPT','OPENAI_API_KEY')
 
+    def GPT_Module(self):
+        client = OpenAI(api_key =self.key)
+
+        completion = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": "Maria DB에 데이터를 넣으려고 하는데 어떻게 하면 좋을까?"}
+        ]
+        )
+
+        print(completion.choices[0].message.content)
+
 def main():
+    module = os.path.basename( sys.argv[0])
+    print(f"{module} Start")
     config_file = sys.argv[1]
     section = sys.argv[2]
-    conf = ConfigParser.ConfigParser()
-    conf.read(config_file)
     
     gpt = ChatGPT(section, config_file)
-    client = OpenAI(api_key =self.key)
+    gpt.GPT_Module()
 
-    completion = client.chat.completions.create(
-    model="gpt-3.5-turbo",
-    messages=[
-        {"role": "system", "content": "You are a poetic assistant, skilled in explaining complex programming concepts with creative flair."},
-        {"role": "user", "content": "Compose a poem that explains the concept of recursion in programming. please answer in korean"}
-    ]
-    )
-
-    print(completion.choices[0].message.content)
+    print("END")
+    
 
 if __name__ == '__main__':
     main()
